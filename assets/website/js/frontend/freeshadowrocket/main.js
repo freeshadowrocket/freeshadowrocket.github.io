f9d2d98586b79f2017764af09b877721a8f58243
yypsa@hotmail.com <yypsa@hotmail.com>
2025-02-07 09:14:55 +0800

    wei yi wei di jiang jia bing bu neng wan quan jie jue wen ti shou ji shu yu di pin gou mai gao pin

--- assets/website/js/frontend/freeshadowrocket/main.js
+++ assets/website/js/frontend/freeshadowrocket/main.js
@@ -188,12 +188,6 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
 
   /**
    * Portfolio details slider
